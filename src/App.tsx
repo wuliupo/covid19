7d4e6995e1d48476406a2a4756a19d33b6b2f536
Puxuan <puxuanhe@gmail.com>
2020-04-15 21:46:58 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Suspense fallback={<div>加载中</div>}>
+      <BrowserRouter>
+        <Switch>
+          {
+            routes.map((props) => (
+              <Route path={props.path}>
+                <props.component />
+              </Route>
+            ))
+          }
+        </Switch>
+      </BrowserRouter>
+    </React.Suspense>
   );
 }
 
---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default [{
+  component: React.lazy(() => import('./pages/index')),
+  path: '/',
+}];
