cea3be87bf9e9957ede44153f9a9f15b29e980e3
liupo.wu <liupo.wu@wz-inc.com>
2020-04-18 22:52:25 +0800

    Updates

---
+++ precache-manifest.ab1aeff2f803187fc97e981220b8ef44.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1308a31cec42821f27dc0fdb647a01ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c13c312ed945bf42401",
+    "url": "/static/css/4.8489c40a.chunk.css"
+  },
+  {
+    "revision": "d9d291f20fc20ffda80d",
+    "url": "/static/css/5.9ea5a966.chunk.css"
+  },
+  {
+    "revision": "fe461f2a5475d670a683",
+    "url": "/static/css/6.a2e3fe4c.chunk.css"
+  },
+  {
+    "revision": "93dd83d26370e082a6fc",
+    "url": "/static/css/7.028092c3.chunk.css"
+  },
+  {
+    "revision": "bd94bb1d2c1cca8de6bb",
+    "url": "/static/css/8.62dc37a9.chunk.css"
+  },
+  {
+    "revision": "8bbe98cf30fb6714deca",
+    "url": "/static/css/9.d5c5812b.chunk.css"
+  },
+  {
+    "revision": "a5fa083bd7364e91dfc9",
+    "url": "/static/css/main.a0814da9.chunk.css"
+  },
+  {
+    "revision": "d2d5e0d452ceaea7433e",
+    "url": "/static/js/0.7a97fa23.chunk.js"
+  },
+  {
+    "revision": "d33aff3b4845d045eeca",
+    "url": "/static/js/1.e7c51ec0.chunk.js"
+  },
+  {
+    "revision": "8709d76cd2a11939df3c",
+    "url": "/static/js/10.6f7279a8.chunk.js"
+  },
+  {
+    "revision": "206b4fbd305bb34eb1645252dbe4e253",
+    "url": "/static/js/10.6f7279a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e401c3b7277c1d0bf3",
+    "url": "/static/js/11.1e3ed945.chunk.js"
+  },
+  {
+    "revision": "1c13c312ed945bf42401",
+    "url": "/static/js/4.b5cdde11.chunk.js"
+  },
+  {
+    "revision": "0154a131a4bbf2986045e88836dad0dc",
+    "url": "/static/js/4.b5cdde11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9d291f20fc20ffda80d",
+    "url": "/static/js/5.8dd92220.chunk.js"
+  },
+  {
+    "revision": "fe461f2a5475d670a683",
+    "url": "/static/js/6.0f9d31cb.chunk.js"
+  },
+  {
+    "revision": "93dd83d26370e082a6fc",
+    "url": "/static/js/7.a3a37888.chunk.js"
+  },
+  {
+    "revision": "bd94bb1d2c1cca8de6bb",
+    "url": "/static/js/8.d3409582.chunk.js"
+  },
+  {
+    "revision": "8bbe98cf30fb6714deca",
+    "url": "/static/js/9.ae3c4b5d.chunk.js"
+  },
+  {
+    "revision": "a5fa083bd7364e91dfc9",
+    "url": "/static/js/main.c58fce67.chunk.js"
+  },
+  {
+    "revision": "0dba3ddbb7330c819584",
+    "url": "/static/js/runtime-main.c06fd805.js"
+  },
+  {
+    "revision": "4e113c7e2f71c27ff4f71c6d1444df6c",
+    "url": "/static/media/virus.4e113c7e.svg"
+  }
+]);
